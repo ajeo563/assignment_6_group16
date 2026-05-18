ce0683a9f150d8e6d627a2b0195a7ff2274b21c0
Andrew <ajeo563@aucklanduni.ac.nz>
2026-05-18 16:08:30 +1200

    Set up SpringBoot server, started on README.

---
+++ src/main/java/codecraft/codecraft/CodecraftApplication.java
@@ -0,0 +1,13 @@
+package codecraft.codecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codecraft/codecraft/CodecraftApplicationTests.java
@@ -0,0 +1,13 @@
+package codecraft.codecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
